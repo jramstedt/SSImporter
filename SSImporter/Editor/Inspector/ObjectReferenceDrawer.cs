a277d14f74c5506fa517d1359ed63d29bcfdd2ef
Janne Ramstedt <janne@sofure.fi>
2015-11-22 22:43:59 +0200

    ObjectReferenceDrawer optimization
    No need for ObjectFactory.

--- SSImporter/Editor/Inspector/ObjectReferenceDrawer.cs
+++ SSImporter/Editor/Inspector/ObjectReferenceDrawer.cs
@@ -7,13 +7,19 @@ using SystemShock;
 
 [CustomPropertyDrawer(typeof(ObjectReferenceAttribute))]
 public class ObjectReferenceDrawer : PropertyDrawer {
-     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
-        ObjectFactory objectFactory = ObjectFactory.GetController();
-        objectFactory.UpdateLevelInfo();
+    public LevelInfo LevelInfo;
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        if (LevelInfo == null)
+            LevelInfo = GameObject.FindObjectOfType<LevelInfo>();
 
         if (property.propertyType == SerializedPropertyType.Integer && property.intValue != 0) {
-            Object obj = objectFactory.Get((ushort)property.intValue);
-            EditorGUI.ObjectField(position, property.displayName, obj, typeof(SystemShockObject), EditorUtility.IsPersistent(obj));
+            SystemShockObject obj;
+            if(LevelInfo.Objects.TryGetValue((ushort)property.intValue, out obj)) {
+                EditorGUI.ObjectField(position, property.displayName, obj, typeof(SystemShockObject), EditorUtility.IsPersistent(obj));
+            } else {
+                EditorGUI.LabelField(position, property.displayName, property.intValue.ToString(), EditorStyles.boldLabel);
+            }
         } else {
             EditorGUI.LabelField(position, property.displayName, property.intValue.ToString(), EditorStyles.boldLabel);
         }
