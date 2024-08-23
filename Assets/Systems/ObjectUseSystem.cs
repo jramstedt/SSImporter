cb359925034a39ca57601f5888e62b09a9bbcc36
jramstedt <janne.ramstedt@aniway.fi>
2024-08-24 01:15:49 +0300

    ObjectUseSystem tease

---
+++ Assets/Systems/ObjectUseSystem.cs
@@ -0,0 +1,19 @@
+using Unity.Burst;
+using Unity.Entities;
+
+namespace SS.System {
+    public partial struct ObjectUseSystem : ISystem {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state) {
+            
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state) { }
+
+        [BurstCompile]
+        public void OnDestroy(ref SystemState state) { }
+    }
+    
+    public struct ObjectUseTag : IComponentData { }
+}
